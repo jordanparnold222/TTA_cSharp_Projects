8df365254edfc8864c9bc025c98210fd33802a5d
jordanparnold222 <42490695+jordanparnold222@users.noreply.github.com>
2021-11-16 02:52:58 -0700

    ACME Accounting push 1

---
+++ ACME_Accounting/ACME_Accounting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACME_Accounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome the ACME Accounting Systems");
+            Console.WriteLine("Remember, we're \"Accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
