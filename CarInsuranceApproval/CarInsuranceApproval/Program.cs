99140a53dbeb913de5b1022ee4a1be50a2b4a83a
jordanparnold222 <42490695+jordanparnold222@users.noreply.github.com>
2021-11-25 17:49:51 -0700

    Car Insurance Approval Submission - 1

---
+++ CarInsuranceApproval/CarInsuranceApproval/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceApproval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //asks for age input
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            //asks for dui history input
+            Console.WriteLine("Have you ever had a DUI? Please reply with either \"true\" or \"false\".");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            //asks for speeding ticket count input
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+
+            //calculates whether the user is qualified
+            bool qualified = (age > 15) && (dui == false) && (tickets <= 3);
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
