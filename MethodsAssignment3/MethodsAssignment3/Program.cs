d531e999926256a1413d1dfe88a1a49ff55c3211
Jordan Arnold <p.jordanarnold@gmail.com>
2021-12-13 10:24:15 -0700

    3rd methods assignment submission - 1

---
+++ MethodsAssignment3/MethodsAssignment3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating the class
+            someMethods methods = new someMethods();
+
+            //Prompting for 1st variable input
+            Console.WriteLine("Enter the first number");
+            int numb1 = Convert.ToInt32(Console.ReadLine());
+
+            //Prompting for 2nd variable input
+            Console.WriteLine("Enter the second number");
+            int numb2 = Convert.ToInt32(Console.ReadLine());
+
+            //Calling the method without specifying the parameter names
+            methods.someMath(numb1, numb2);
+
+            //Calling the method while specifying the parameter names
+            methods.someMath(num1: numb1, num2: numb2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodsAssignment3/MethodsAssignment3/someMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment3
+{
+    class someMethods
+    {
+
+        public void someMath(int num1, int num2)
+        {
+            int num3 = num1 + num1;
+            Console.WriteLine(num2);
+        }
+    }
+}
