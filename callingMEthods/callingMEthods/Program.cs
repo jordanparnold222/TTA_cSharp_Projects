c457f7bf84b640a9954387abe0d60cd6cde84dd8
Jordan Arnold <p.jordanarnold@gmail.com>
2021-12-13 08:49:38 -0700

    methods submission - 1

---
+++ callingMEthods/callingMEthods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace callingMEthods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompting user to enter the number
+            Console.WriteLine("Hi, enter a number and I will perform 3 math operations on it.");
+            int chosenNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Displaying the results of the methods called from the someMethods class
+            Console.WriteLine("Your number after doubling it is: " + someMethods.Doubling(chosenNumber));
+            Console.WriteLine("Your number after being cut in half is: " + someMethods.Half(chosenNumber));
+            Console.WriteLine("Your number after multiplying it by itself is: " + someMethods.MultByItself(chosenNumber));
+            Console.Read();
+        }
+    }
+}
---
+++ callingMEthods/callingMEthods/someMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace callingMEthods
+{
+    public static class someMethods
+    {
+        //doubling method
+        public static int Doubling(int num1)
+        {
+            int num2 = num1 * 2;
+            return num2;
+        }
+
+        //halfing method
+        public static int Half(int num1)
+        {
+            int num2 = num1 / 2;
+            return num2;
+        }
+
+        //squaring method
+        public static int MultByItself(int num1)
+        {
+            int num2 = num1 + num1;
+            return num2;
+        }
+    }
+}
