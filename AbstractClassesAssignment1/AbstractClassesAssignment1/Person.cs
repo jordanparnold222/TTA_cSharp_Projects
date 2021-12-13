4368717f8e9be3ebbb163bc830a98c1e17fab1ca
Jordan Arnold <p.jordanarnold@gmail.com>
2021-12-13 12:59:55 -0700

    Abstract assignment 1 submission - 1

---
+++ AbstractClassesAssignment1/AbstractClassesAssignment1/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesAssignment1
+{
+    //instantiation of the abstract "person" class
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        //prints the person's first and last name to the screen
+        public void SayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AbstractClassesAssignment1/AbstractClassesAssignment1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating an object of the "employee" class
+            Employee emp1 = new Employee();
+
+            //assigning values the the object's class attributes
+            emp1.firstName = "Sample";
+            emp1.lastName = "Student";
+
+            //calling the "sayname" function that was inhereted from the abstract "person" class
+            emp1.SayName();
+        }
+    }
+}
