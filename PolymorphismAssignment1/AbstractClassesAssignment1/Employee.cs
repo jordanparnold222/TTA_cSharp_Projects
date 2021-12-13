2b108d7f05ade114d82daa52676a099622f75a25
Jordan Arnold <p.jordanarnold@gmail.com>
2021-12-13 14:37:42 -0700

    Polymorphism assignment 1 submission - 1

---
+++ PolymorphismAssignment1/AbstractClassesAssignment1/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesAssignment1
+{
+    //enheriting from the abstract "person" class
+    class Employee : Person, IQuittable
+    {
+
+        //Adding to the SawName Method that was inhereted
+        public override void SayName()
+        {
+            Console.WriteLine("Employee name:");
+            base.SayName();
+        }
+
+        //Adding functionality to the Quit method that was inhereted from the IQuittable interface
+        public void Quit()
+        {
+            Console.WriteLine("You quit your job");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PolymorphismAssignment1/AbstractClassesAssignment1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating an object of the "employee" class
+            Employee emp1 = new Employee();
+
+            //assigning values the the object's class attributes
+            emp1.firstName = "Sample";
+            emp1.lastName = "Student";
+
+            //calling the "sayname" function that was inhereted from the abstract "person" class
+            emp1.SayName();
+
+            //Using polymorphism to instantiate an object that has the type IQuittable
+            IQuittable IQuit = new Employee();
+
+           
+            IQuit.Quit();
+        }
+    }
+}
