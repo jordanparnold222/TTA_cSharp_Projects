8f5179f8100174a206375eb821d290c9cc321b80
Jordan Arnold <p.jordanarnold@gmail.com>
2021-12-13 10:14:18 -0700

    2nd methods assignment submission - 1

---
+++ MethodsSubmission2/MethodsSubmission2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsSubmission2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiating the class
+            someMethods methods = new someMethods();
+
+            //Getting input for the one required parameter
+            Console.WriteLine("Enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} + 3 equals: {1}", num1, methods.Adding(num1));
+            Console.ReadLine();
+
+            //Giving the user the option to modify the second parameter from its default value
+            Console.WriteLine("Enter a second number if you want. This isn't required.");
+            string num2 = Console.ReadLine();
+
+            //Determining whether or not the user decided to re-run the method with its second parameter modified
+            if (num2 != "")
+            {
+                int num2Conv = Convert.ToInt32(num2);
+
+                Console.WriteLine("{0} plus {1} equals {2}", num1, num2Conv, methods.Adding(num1, num2Conv));
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine("Alright, have a nice day.");
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ MethodsSubmission2/MethodsSubmission2/someMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsSubmission2
+{
+    class someMethods
+    {
+
+        //The method used for this assignment
+        public int Adding(int num1, int num2 = 3)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+    }
+}
