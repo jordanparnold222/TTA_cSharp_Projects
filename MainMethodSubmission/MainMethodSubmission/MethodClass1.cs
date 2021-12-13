8e1d8515b61f8920ac3df418f2cb889cfb8e687b
Jordan Arnold <p.jordanarnold@gmail.com>
2021-12-13 09:36:00 -0700

    Main methods submission - 1

---
+++ MainMethodSubmission/MainMethodSubmission/MethodClass1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class MethodClass1
+    {
+        //Original method
+        public int Adding(int num1)
+        {
+            int num2 = num1 + num1;
+            return num2;
+        }
+
+        //2nd method when the double argument is passed in
+        public int Adding(int num1, double num2)
+        {
+            int result = num1 + Convert.ToInt32(num2);
+            return result;
+        }
+
+        //3rd method when all 3 possible arguments are passed in
+        public int Adding(int num1, double num2, string num3)
+        {
+            int strToNum = Convert.ToInt32(num3);
+            int result = num1 + Convert.ToInt32(num2) + strToNum;
+            return result;
+        }
+    }
+}
---
+++ MainMethodSubmission/MainMethodSubmission/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating the methods class I created
+            MethodClass1 methods = new MethodClass1();
+
+            //Utilizing 1st instance of Adding method
+            Console.WriteLine("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The result of adding that number to itself is: {0}", methods.Adding(num1));
+            Console.ReadLine();
+
+            //Utilizing the 2nd instance of Adding method but including the second argument
+            Console.WriteLine("Now enter a second number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("When adding this number to the first number, after rounding, we get: {0}", methods.Adding(num1, num2));
+            Console.ReadLine();
+
+            //Utilizing the 3rd instance of Adding method by also including the optional string variable
+            Console.WriteLine("Now enter a third number");
+            string num3 = Console.ReadLine();
+
+            Console.WriteLine("When adding the third number to the other two, we get: {0}", methods.Adding(num1, num2, num3));
+            Console.ReadLine();
+        }
+    }
+}
