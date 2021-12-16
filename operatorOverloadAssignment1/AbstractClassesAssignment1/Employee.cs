7319b6481140cd1119840319d14f4f8c3f4a4f0a
Jordan Arnold <p.jordanarnold@gmail.com>
2021-12-16 05:08:46 -0700

    operator overload submission - 2

---
+++ operatorOverloadAssignment1/AbstractClassesAssignment1/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesAssignment1
+{
+    //enheriting from the abstract "person" class
+    class Employee : Person, IQuittable
+    {
+
+
+        public int ID { get; set; }
+        //Adding to the SayName Method that was inhereted
+        public override void SayName()
+        {
+            Console.WriteLine("Employee name:");
+            base.SayName();
+        }
+
+        //Adding functionality to the Quit method that was inhereted from the IQuittable interface
+        public void Quit()
+        {
+            Console.WriteLine("You quit your job");
+            Console.ReadLine();
+        }
+
+
+        //These next 2 overloads are for the == and != operators to compare 2 employees based off ther ID numbers
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+
+
+            return emp1.ID == emp2.ID;
+        }
+
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+
+
+            return emp1.ID != emp2.ID;
+        }
+    }
+}
---
+++ operatorOverloadAssignment1/AbstractClassesAssignment1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1st employee instantiation and assignning of values
+            Employee emp1 = new Employee();
+
+            emp1.firstName = "Sample";
+            emp1.lastName = "Student";
+            emp1.ID = 1;
+
+            //2nd employee instantiation and assignning of values
+            Employee emp2 = new Employee();
+            emp2.firstName = "Student";
+            emp2.lastName = "Sample";
+            emp2.ID = 1;
+
+
+            //testing the == overload I created in the employee class
+            Console.WriteLine(emp2 == emp1);
+            Console.ReadLine();
+
+
+
+
+
+            //*****EVERYTHING BELOW THIS IS IRRELEVANT TO THE CURRENT SUBMISSION*****//
+
+
+
+            ////calling the "sayname" function that was inhereted from the abstract "person" class
+            //emp1.SayName();
+
+            ////Using polymorphism to instantiate an object that has the type IQuittable
+            //IQuittable IQuit = new Employee();
+
+            //List<Employee> emp = new List<Employee>();
+
+
+           
+            //IQuit.Quit();
+        }
+    }
+}
