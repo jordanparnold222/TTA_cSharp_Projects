204da999ea54f38adc7500f7ae4fe8dab90ec76c
Jordan Arnold <p.jordanarnold@gmail.com>
2021-12-26 11:49:39 -0700

    Constructors assignment submission - 1

---
+++ ConstructorAssignment1/ConstructorAssignment1/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment1
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+
+
+        //Chained constructor
+        public Person () : this("Jordan", 25)
+        {
+
+        }
+        //Original constructor
+        public Person(string theName, int theAge)
+        {
+            name = theName;
+            age = theAge;
+        }
+    }
+}
---
+++ ConstructorAssignment1/ConstructorAssignment1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //constant variable
+            const string name = "Jordan";
+            //implicit variable
+            var thing = "thing";
+
+            //Testing the make sure C# recognized the implicit data type
+            Console.WriteLine(thing.GetType());
+            Console.ReadLine();
+
+            //Testing the constructors
+            Person person1 = new Person();
+            Person person2 = new Person("John", 75);
+            Console.WriteLine(person1.name + " " + person1.age);
+            Console.WriteLine(person2.name + " " + person2.age);
+            Console.ReadLine();
+
+
+        }
+    }
+}
