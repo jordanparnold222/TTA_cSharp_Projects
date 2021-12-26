97a29eb5026bb7c7a7f33653b6e26c1536414d07
Jordan Arnold <p.jordanarnold@gmail.com>
2021-12-26 15:09:32 -0700

    Try Catch Assignment Submission - 1

---
+++ TryCatchAssignment2/TryCatchAssignment2/InvalidAgeException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment2
+{
+    //Inhereting from the 'Exception' class so that it can mimics its behavior
+    public class InvalidAgeException : Exception
+    {
+        //creating the constructor for use in the main program.
+        public InvalidAgeException()
+            : base() { }
+        public InvalidAgeException(string message)
+            : base(message) { }
+    }
+}
---
+++ TryCatchAssignment2/TryCatchAssignment2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Determines whether the loop continues
+            bool validAge = false;
+            while (!validAge)
+            {
+                //attempt to recieve a proper age
+                try
+                {
+                    Console.WriteLine("Hi, how old are you?");
+                    int age = Convert.ToInt32(Console.ReadLine());
+                    if (age < 1) throw new InvalidAgeException();
+
+                    //If the input was valid, the program will continue as planned
+                    else
+                    {
+                        int year = 2021 - age;
+                        Console.WriteLine("Ah, so you were born in {0}.", year);
+                        Console.ReadLine();
+                        validAge = true;
+                    }
+
+                }
+                //Catching for an input value of less than 1
+                catch (InvalidAgeException)
+                {
+                    Console.WriteLine("That is not physically possible. Try again.");
+                    Console.ReadLine();
+
+                }
+                //catching any other exception that wasn't already explicitely stated.
+                catch (Exception)
+                {
+                    Console.WriteLine("Not sure what happened there. Let's try again.");
+                    Console.ReadLine();
+                }
+
+
+            }
+           
+
+        }
+    }
+}
