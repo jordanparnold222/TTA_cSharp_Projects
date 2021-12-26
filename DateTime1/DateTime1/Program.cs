e4e3e22a1e068aeff12ec64f1905e99aab163f5c
Jordan Arnold <p.jordanarnold@gmail.com>
2021-12-25 19:57:31 -0700

    DateTime assignment submission - 1

---
+++ DateTime1/DateTime1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Showing current date and time
+            Console.WriteLine("The current time is: " + DateTime.Now);
+            Console.ReadLine();
+
+            //getting user input, to add to the current time
+            Console.WriteLine("Enter a number");
+            int answer = Convert.ToInt32(Console.ReadLine());
+
+
+            //adding that input as hours to the current date and time
+            Console.WriteLine("Wow. That is " + DateTime.Now.AddHours(answer) + " if we added that to the current time as hours.");
+            Console.ReadLine();
+        }
+    }
+}
