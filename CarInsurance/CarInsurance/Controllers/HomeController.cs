9e78722b4b9fee410665f9ed0a68c78ada813f88
Jordan Arnold <p.jordanarnold@gmail.com>
2022-01-04 15:08:44 -0700

    Car Insurance Submission - 1

---
+++ CarInsurance/CarInsurance/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using CarInsurance.Models;
+using CarInsurance.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Admin()
+        {
+            using (var db = new InsuranceEntities())
+            {
+                var insurees = db.Insurees;
+                var insureeVms = new List<InsureeVm>();
+                foreach (var insuree in insurees)
+                {
+                    var insureeVm = new InsureeVm();
+                    insureeVm.FirstName = insuree.FirstName;
+                    insureeVm.LastName = insuree.LastName;
+                    insureeVm.EmailAddress = insuree.EmailAddress;
+
+                    insureeVms.Add(insureeVm);
+                }
+
+                return View(insureeVms);
+            }
+        }
+    }
+}
