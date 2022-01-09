00c24c86704bbebd6e09a20867f3d3d252f19f26
Jordan Arnold <p.jordanarnold@gmail.com>
2022-01-09 11:07:31 -0700

    Student Database Code-First Submission - 1

---
+++ StudentDatabase/StudentDatabase/Program.cs
@@ -0,0 +1,20 @@
+﻿using StudentDatabase.DataModels;
+using System;
+
+namespace StudentDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var student = new Student() { StudentName = "Jordan" };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+
+        }
+    }
+}
